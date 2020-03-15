dd41df3bc29b85a6fa4b214afda284c176987f1e
Alejandro Barrachina Argudo <44205191+ALK222@users.noreply.github.com>
2020-03-15 10:33:14 +0100

    Vehicle class and event now use Junctions

--- Traffic_Simulator/src/simulator/model/NewVehicleEvent.java
+++ Traffic_Simulator/src/simulator/model/NewVehicleEvent.java
@@ -1,5 +1,6 @@
 package simulator.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import exceptions.RoadException;
@@ -8,15 +9,32 @@ import exceptions.VehicleException;
 public class NewVehicleEvent extends Event {
 
 	private Vehicle v;
+
+	private String id;
+
+	private int maxSpeed;
+	
+	private int contClass;
+
+	private List<String> itinerary;
 	
 	
 	public NewVehicleEvent(int time, String id, int maxSpeed, int contClass, List<String> itinerary) throws VehicleException {
 		super(time);
-		this.v = new Vehicle(id, maxSpeed, contClass, itinerary);
+		this.id = id;
+		this.maxSpeed = maxSpeed;
+		this.contClass = contClass;
+		this.itinerary = itinerary;
 	}
 
 	@Override
-	void execute(RoadMap map) throws RoadException {
+	void execute(RoadMap map) throws RoadException, VehicleException {
+		List<Junction> vItinerary = new ArrayList<Junction>();
+		for(String i : itinerary){
+			Junction j = map.getJunction(i);
+			vItinerary.add(j);
+		}
+		v = new Vehicle(id, maxSpeed, contClass, vItinerary);
 		v.moveToNextRoad();
 
 	}
--- Traffic_Simulator/src/simulator/model/Vehicle.java
+++ Traffic_Simulator/src/simulator/model/Vehicle.java
@@ -12,7 +12,7 @@ public class Vehicle extends SimulatedObject{
 	
 	//ATRIBUTTES
 	
-	private List<String> itinerary;
+	private List<Junction> itinerary;
 	
 	private int max_speed;
 	
@@ -32,7 +32,7 @@ public class Vehicle extends SimulatedObject{
 	
 	//CONSTRUCTOR
 	
-	Vehicle(String id, int maxSpeed, int contClass, List<String> itinerary) throws VehicleException {
+	Vehicle(String id, int maxSpeed, int contClass, List<Junction> itinerary) throws VehicleException {
 		super(id);
 		if(maxSpeed < 0) {
 			throw new VehicleException("Invalid speed");
